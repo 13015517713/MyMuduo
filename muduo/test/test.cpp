5324877763a8e4cf23e8528fe5a696c84c550ca2
13015517713 <2430278602@qq.com>
2020-12-22 21:51:03 +0800

    add thread.cpp

--- muduo/test/test.cpp
+++ muduo/test/test.cpp
@@ -1,8 +1,25 @@
-#include "Logger.h"
+/*
+我需要弄懂的几个点：
+（1）创建线程传参数&都传了什么
+（2）创建线程用lambda表达式 这点 lambda表达式作用这么大吗？
+*/
+#include <thread>
 #include <iostream>
-
 using namespace std;
 
+class Thread{
+public:
+    int a = 1;
+    int b = 2;
+    void createThread(){
+        std::thread _thread(run,this);
+    }
+    void run(Thread *t){
+        std::cout << "run test." << std::endl;
+        std::cout << t->a << std::endl;
+    }
+};
+
 int main(){
-    LogINFO("%s","test");
+    Thread().createThread();
 }
